fe4d4bd82f96bdf51866602455377c2adc6605b3
ZacharyLandes <zach.g.landes@gmail.com>
2017-12-20 16:04:20 -0700

    porduction file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.serve_static_assets = true
   config.assets.compile = true
 
-config.assets.digest = true
+  config.assets.digest = true
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -31,7 +31,6 @@ config.assets.digest = true
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
