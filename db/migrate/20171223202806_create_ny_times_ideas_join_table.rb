514e477b5bc9eb8f4b2fb2c9b1f3af299b7fd26b
ZacharyLandes <zach.g.landes@gmail.com>
2017-12-23 23:39:25 -0700

    reopen heroku app

--- db/migrate/20171223202806_create_ny_times_ideas_join_table.rb
+++ db/migrate/20171223202806_create_ny_times_ideas_join_table.rb
@@ -1,6 +1,5 @@
 class CreatenytimesIdeasJoinTable < ActiveRecord::Migration[5.1]
   def change
          create_join_table :ideas, :nytimes
-
   end
 end
--- db/migrate/20171223204208_rename_ny_timesto_nytimes.rb
+++
@@ -1,6 +0,0 @@
-class RenameNyTimestoNytimes < ActiveRecord::Migration[5.1]
-  def change
-         rename_table :ny_times, :nytimes
-
-  end
-end
